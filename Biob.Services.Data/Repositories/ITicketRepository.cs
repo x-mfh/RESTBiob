7a5045aa95d3f8dc5d2b9a44d0b2701b84f292a4
BT <bjarke.treufeldt@gmail.com>
2018-11-20 11:11:06 +0100

    Added ticket interface

---
+++ Biob.Services.Data/Repositories/ITicketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biob.Services.Data.Repositories
+{
+    public interface ITicketRepository : IRepository
+    {
+        Task<Ticket> GetTicketAsync(Guid id);
+        void AddTicket(Ticket ticketToAdd);
+        void UpdateTicket(Ticket ticketToUpdate);
+        void DeleteTicket(Ticket ticketToDelete);
+    }
+}
