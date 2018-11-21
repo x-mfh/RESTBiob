cf340bab317f656f8df21dd35c151d67576a2114
sabeniano <jason_tolentino@hotmail.com>
2018-11-21 11:45:37 +0100

    Showtime repos and controller partially done

---
+++ Biob.Services.Data/DtoModels/ShowtimeToCreateDto .cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Biob.Services.Data.DtoModels
+{
+    public class ShowtimeToCreateDto
+    {
+        public Guid Id { get; set; }
+        public Guid MovieId { get; set; }
+        public int HallId { get; set; }
+        public DateTimeOffset TimeOfPlaying { get; set; }
+        public bool ThreeDee { get; set; }
+    }
+}
