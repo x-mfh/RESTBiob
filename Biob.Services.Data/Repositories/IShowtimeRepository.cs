05a492c05c5650472f057c8fd83bb745b2f4225e
sabeniano <jason_tolentino@hotmail.com>
2018-11-21 11:45:37 +0100

    Showtime repos and controller partially done

---
+++ Biob.Services.Data/Repositories/IShowtimeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Biob.Data.Models;
+
+namespace Biob.Services.Data.Repositories
+{
+    public interface IShowtimeRepository : IRepository
+    {
+        Task<IEnumerable<Showtime>> GetAllShowtimesAsync();
+        Task<Showtime> GetShowtimeAsync(Guid showtimeId);
+        void AddShowtime(Showtime showtimeToAdd);
+        void UpdateShowtime(Showtime showtimeToUpdate);
+        void DeleteShowtime(Showtime showtimeToDelete);
+    }
+}
