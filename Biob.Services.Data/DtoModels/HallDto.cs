35e45cc8c3950f191dce9e248a88f264779e9b6a
xmfh <mikkel_fhammer@hotmail.com>
2018-11-21 12:29:40 +0100

    fully added hall, new constraint on HallNo commented out

---
+++ Biob.Services.Data/DtoModels/HallDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biob.Services.Data.DtoModels
+{
+    public class HallDto
+    {
+        public int Id { get; set; }
+        public int HallNo { get; set; }
+        public int NoOfSeats { get; set; }
+        public bool ThreeDee { get; set; }
+    }
+}
