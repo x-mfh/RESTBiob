202e82e7f48182bdf010e7a201cbb7a74ec1b7a1
Mikkel H <mikkel_fhammer@hotmail.com>
2018-11-23 20:11:12 +0100

    added migration for wrapper class

--- Biob.Data.Models/Hall.cs
+++ Biob.Data.Models/Hall.cs
@@ -10,9 +10,7 @@ namespace Biob.Data.Models
     [Table("Halls")]
     public class Hall : DeleteableModelBase<int>
     {
-        [Key]
         //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
         [Required]
         public int HallNo { get; set; }
         public int NoOfSeats { get; set; }
