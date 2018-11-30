7c4e282e3cf706af6b7105bdc2ab96ed58329db7
sabeniano <jason_tolentino@hotmail.com>
2018-11-30 10:17:41 +0100

    Changed hallId from int to Guid in Update-, Create- and ShowtimeDTO

--- Biob.Services.Data/DtoModels/ShowtimeToUpdateDto.cs
+++ Biob.Services.Data/DtoModels/ShowtimeToUpdateDto.cs
@@ -5,7 +5,7 @@ namespace Biob.Services.Data.DtoModels
     public class ShowtimeToUpdateDto
     {
         public Guid MovieId { get; set; }
-        public int HallId { get; set; }
+        public Guid HallId { get; set; }
         public DateTimeOffset TimeOfPlaying { get; set; }
         public bool ThreeDee { get; set; }
     }
