92eb3be24ecd3a49d573fc72c708d837b0e94ba5
BalenD <balen1996@hotmail.com>
2018-11-30 11:00:10 +0100

    changed a bug in movie repository

--- Biob.Services.Data/Repositories/MovieRepository.cs
+++ Biob.Services.Data/Repositories/MovieRepository.cs
@@ -65,7 +65,7 @@ namespace Biob.Services.Data.Repositories
         public async Task<Movie> GetMovieAsync(Guid id)
         {
             var foundMovie = await _context.Movies.Where(movie => movie.Id == id).FirstOrDefaultAsync();
-            if (foundMovie.IsDeleted)
+            if (foundMovie != null && foundMovie.IsDeleted)
             {
                 foundMovie = null;
             }
