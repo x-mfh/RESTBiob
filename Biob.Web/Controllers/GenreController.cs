54d058d0ad61b1414dcc39bbda7ec087f4079b52
Mikkel H <mikkel_fhammer@hotmail.com>
2018-12-02 16:59:34 +0100

    Added hateoas to genre and hall

--- Biob.Web/Controllers/GenreController.cs
+++ Biob.Web/Controllers/GenreController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Biob.Data.Models;
 using Biob.Services.Data.DtoModels;
+using Biob.Services.Data.Helpers;
 using Biob.Services.Data.Repositories;
 using Biob.Web.Helpers;
 using Microsoft.AspNetCore.JsonPatch;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,31 +20,53 @@ namespace Biob.Web.Controllers
     public class GenreController : ControllerBase
     {
         private readonly IGenreRepository _genreRepository;
+        private readonly IUrlHelper _urlHelper;
         private readonly ILogger<GenreController> _logger;
 
-        public GenreController(IGenreRepository genreRepository, ILogger<GenreController> logger)
+        public GenreController(IGenreRepository genreRepository, IUrlHelper urlHelper, ILogger<GenreController> logger)
         {
             _genreRepository = genreRepository;
+            _urlHelper = urlHelper;
             _logger = logger;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllGenres()
+        public async Task<IActionResult> GetAllGenres([FromHeader(Name = "Accept")] string mediaType)
         {
-            var genresFound = await _genreRepository.GetAllGenresAsync();
+            var genres = await _genreRepository.GetAllGenresAsync();
 
-            var mappedGenres = Mapper.Map<IEnumerable<GenreDto>>(genresFound);
+            var mappedGenres = Mapper.Map<IEnumerable<GenreDto>>(genres);
+
+            if (mediaType == "application/vnd.biob.json+hateoas")
+            {
+                return Ok(CreateLinksForGenres(mappedGenres));
+            }
 
             return Ok(mappedGenres);
         }
 
         [HttpGet("{genreId}", Name = "GetGenre")]
-        public async Task<IActionResult> GetGenreById([FromRoute] Guid genreId)
+        public async Task<IActionResult> GetGenreById([FromRoute] Guid genreId, [FromHeader(Name = "Accept")] string mediaType)
         {
             var genreFound = await _genreRepository.GetGenreByIdAsync(genreId);
 
+            if (genreFound == null)
+            {
+                return NotFound();
+            }
+
             var mappedGenre = Mapper.Map<GenreDto>(genreFound);
 
+            if (mediaType == "application/vnd.biob.json+hateoas")
+            {
+                var links = CreateLinksForGenre(genreId);
+
+                var linkedGenre = mappedGenre.ShapeData(null) as IDictionary<string, object>;
+
+                linkedGenre.Add("links", links);
+
+                return Ok(linkedGenre);
+            }
             return Ok(mappedGenre);
         }
 
@@ -73,7 +97,8 @@ namespace Biob.Web.Controllers
             return CreatedAtRoute("GetGenre", new { genreId = genreToReturn.Id }, genreToReturn);
         }
 
-        [HttpPatch("{genreId}")]
+        [HttpPatch("{genreId}", Name = "PartiallyUpdateGenre")]
+        [HttpPut("{genreId}", Name = "UpdateGenre")]
         public async Task<IActionResult> UpdateGenre([FromRoute] Guid genreId, JsonPatchDocument<GenreToUpdateDto> patchDoc)
         {
             if (patchDoc == null)
@@ -113,7 +138,7 @@ namespace Biob.Web.Controllers
         }
 
 
-        [HttpDelete("{genreId}")]
+        [HttpDelete("{genreId}", Name = "DeleteGenre")]
         public async Task<IActionResult> DeleteGenre([FromRoute] Guid genreId)
         {
             var genreToDelete = await _genreRepository.GetGenreByIdAsync(genreId);
@@ -131,6 +156,37 @@ namespace Biob.Web.Controllers
             }
 
             return NoContent();
+        }
+
+        private IEnumerable<LinkDto> CreateLinksForGenre(Guid id)
+        {
+            var links = new List<LinkDto>
+            {
+                new LinkDto(_urlHelper.Link("GetGenre", new { genreId = id }), "self", "GET"),
+                new LinkDto(_urlHelper.Link("UpdateGenre", new { genreId = id }), "update_genre", "PUT"),
+                new LinkDto(_urlHelper.Link("PartiallyUpdateGenre", new { genreId = id }), "partially_update_genre", "PATCH"),
+                new LinkDto(_urlHelper.Link("DeleteGenre", new { genreId = id }), "delete_genre", "DELETE")
+            };
+
+            return links;
+        }
+
+        private ExpandoObject CreateLinksForGenres(IEnumerable<GenreDto> genreList)
+        {
+            var shapedGenres = genreList.ShapeData(null);
+            var genresWithLinks = shapedGenres.Select(genre =>
+            {
+                var genreDictionary = genre as IDictionary<string, object>;
+                var genreLinks = CreateLinksForGenre((Guid)genreDictionary["Id"]);
+
+                genreDictionary.Add("links", genreLinks);
+
+                return genreDictionary;
+            });
+            var linkedCollection = new ExpandoObject();
+            ((IDictionary<string, object>)linkedCollection).Add("genres", genresWithLinks);
+
+            return linkedCollection;
         }
     }
 }
