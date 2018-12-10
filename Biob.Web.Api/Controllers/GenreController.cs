a3241cf0d477e418b28de6fad0da4a7c9dd6a6b6
Mikkel H <mikkel_fhammer@hotmail.com>
2018-12-10 17:40:27 +0100

    added swagger documention to hall/genre controller, added hateoas response on upsert, plus minor fixes

--- Biob.Web.Api/Controllers/GenreController.cs
+++ Biob.Web.Api/Controllers/GenreController.cs
@@ -7,6 +7,7 @@ using Biob.Web.Api.Filters;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
@@ -30,8 +31,16 @@ namespace Biob.Web.Api.Controllers
             _logger = logger;
         }
 
+        [SwaggerOperation(
+            Summary = "Retrieve every genres",
+            Description = "Retrieves every genres in the database",
+            Consumes = new string[] { },
+            Produces = new string[] { "application/json", "application/vnd.biob.json+hateoas" })]
+        [SwaggerResponse(200, "Successfully retrieved every genre", typeof(GenreDto[]))]
+        [SwaggerResponse(400, "Request data is invalid", null)]
         [HttpGet]
-        public async Task<IActionResult> GetAllGenresAsync([FromHeader(Name = "Accept")] string mediaType)
+        public async Task<IActionResult> GetAllGenresAsync(
+            [FromHeader(Name = "Accept"), SwaggerParameter(Description = "media type to request between json or json+hateoas")] string mediaType)
         {
             var genres = await _genreRepository.GetAllGenresAsync();
 
@@ -45,9 +54,18 @@ namespace Biob.Web.Api.Controllers
             return Ok(mappedGenres);
         }
 
+        [SwaggerOperation(
+            Summary = "Retrieve one genre by ID",
+            Description = "Retrieves genre in the database by id",
+            Consumes = new string[] { },
+            Produces = new string[] { "application/json", "application/vnd.biob.json+hateoas" })]
+        [SwaggerResponse(200, "Successfully retrieved a genre", typeof(GenreDto))]
+        [SwaggerResponse(400, "Request data is invalid", null)]
         [HttpGet("{genreId}", Name = "GetGenre")]
         [GuidCheckActionFilter(new string[] { "genreId" })]
-        public async Task<IActionResult> GetGenreByIdAsync([FromRoute] Guid genreId, [FromHeader(Name = "Accept")] string mediaType)
+        public async Task<IActionResult> GetGenreByIdAsync(
+            [FromRoute, SwaggerParameter(Description = "the ID to find genre by", Required = true)] Guid genreId, 
+            [FromHeader(Name = "Accept"), SwaggerParameter(Description = "media type to request between json or json+hateoas")] string mediaType)
         {
             var genreFound = await _genreRepository.GetGenreByIdAsync(genreId);
 
@@ -71,27 +89,54 @@ namespace Biob.Web.Api.Controllers
             return Ok(mappedGenre);
         }
 
+        [SwaggerOperation(
+            Summary = "Create a genre",
+            Description = "Creates a genre in the database",
+            Consumes = new string[] { "application/json" },
+            Produces = new string[] { "application/json", "application/vnd.biob.json+hateoas" })]
+        [SwaggerResponse(200, "Successfully created a genre", typeof(GenreDto))]
         [HttpPost]
-        public async Task<IActionResult> CreateGenreAsync(GenreToCreateDto genreToCreate)
+        public async Task<IActionResult> CreateGenreAsync(
+            [FromBody, SwaggerParameter(Description = "Genre to create", Required = true)]GenreToCreateDto genreToCreate,
+            [FromHeader(Name = "Accept"), SwaggerParameter(Description = "media type to request between json or json+hateoas")] string mediaType)
         {
-
             var genreToAdd = Mapper.Map<Genre>(genreToCreate);
             _genreRepository.AddGenre(genreToAdd);
-
-
-            // mapped created genre back to genreDto to remove unnecessary information
-            var genreToReturn = Mapper.Map<GenreDto>(genreToAdd);
 
             if (!await _genreRepository.SaveChangesAsync())
             {
                 _logger.LogError("Saving changes to database while creating a genre failed");
             }
-            return CreatedAtRoute("GetGenre", new { genreId = genreToReturn.Id }, genreToReturn);
+
+            // mapped created genre back to genreDto to remove unnecessary information
+            var genreToReturn = Mapper.Map<GenreDto>(genreToAdd);
+
+            if (mediaType == "application/vnd.biob.json+hateoas")
+            {
+                var links = CreateLinksForGenre(genreToReturn.Id);
+
+                var linkedGenre = genreToReturn.ShapeData(null) as IDictionary<string, object>;
+
+                linkedGenre.Add("links", links);
+
+                return CreatedAtRoute("GetGenre", new { genreId = genreToReturn.Id }, linkedGenre);
+            }
+        
+                return CreatedAtRoute("GetGenre", new { genreId = genreToReturn.Id }, genreToReturn);
         }
 
+        [SwaggerOperation(
+            Summary = "Partially update a genre",
+            Description = "Partially updates a genre in the database",
+            Consumes = new string[] { "application/json-patch+json" },
+            Produces = new string[] { "application/json", "application/vnd.biob.json+hateoas" })]
+        [SwaggerResponse(200, "Successfully partially updated a genre", typeof(GenreDto))]
+        [SwaggerResponse(400, "Request data is invalid", null)]
         [HttpPatch("{genreId}", Name = "PartiallyUpdateGenre")]
         [GuidCheckActionFilter(new string[] { "genreId" })]
-        public async Task<IActionResult> PartiallyUpdateGenreAsync([FromRoute] Guid genreId, JsonPatchDocument<GenreToUpdateDto> patchDoc)
+        public async Task<IActionResult> PartiallyUpdateGenreAsync(
+            [FromRoute, SwaggerParameter(Description = "Id of genre to update", Required = true)] Guid genreId,
+            [FromBody, SwaggerParameter(Description = "Jsonpatch operation document to update", Required = true)] JsonPatchDocument<GenreToUpdateDto> patchDoc)
         {
             if (patchDoc == null)
             {
@@ -117,10 +162,18 @@ namespace Biob.Web.Api.Controllers
 
         }
 
-
+        [SwaggerOperation(
+            Summary = "Update a genre",
+            Description = "Updates a genre in the database",
+            Consumes = new string[] { "application/json" },
+            Produces = new string[] { "application/json", "application/vnd.biob.json+hateoas" })]
+        [SwaggerResponse(200, "Successfully updated a genre", typeof(GenreDto))]
+        [SwaggerResponse(400, "Request data is invalid", null)]
         [HttpPut("{genreId}", Name = "UpdateGenre")]
         [GuidCheckActionFilter(new string[] { "genreId" })]
-        public async Task<IActionResult> UpdateGenreAsync([FromRoute] Guid genreId, [FromBody] GenreToUpdateDto genreToUpdate)
+        public async Task<IActionResult> UpdateGenreAsync(
+            [FromRoute, SwaggerParameter(Description = "The id of genre to update", Required = true)] Guid genreId, 
+            [FromBody, SwaggerParameter(Description = "Genre to update", Required = true)] GenreToUpdateDto genreToUpdate)
         {
             var genreFromDb = await _genreRepository.GetGenreByIdAsync(genreId);
 
@@ -137,10 +190,17 @@ namespace Biob.Web.Api.Controllers
 
         }
 
-
+        [SwaggerOperation(
+            Summary = "Hard deletes a genre",
+            Description = "Hard deletes a genre in the database",
+            Consumes = new string[] { },
+            Produces = new string[] { "application/json", "application/vnd.biob.json+hateoas" })]
+        [SwaggerResponse(200, "Successfully deleted a genre", null)]
+        [SwaggerResponse(400, "Request data is invalid", null)]
         [HttpDelete("{genreId}", Name = "DeleteGenre")]
         [GuidCheckActionFilter(new string[] { "genreId" })]
-        public async Task<IActionResult> DeleteGenreAsync([FromRoute] Guid genreId)
+        public async Task<IActionResult> DeleteGenreAsync(
+            [FromRoute, SwaggerParameter(Description = "Id of genre to delete", Required = true)] Guid genreId)
         {
             var genreToDelete = await _genreRepository.GetGenreByIdAsync(genreId);
 
@@ -159,6 +219,12 @@ namespace Biob.Web.Api.Controllers
             return NoContent();
         }
 
+        [SwaggerOperation(
+            Summary = "Get option information",
+            Description = "Gets HTTP methods options for this route",
+            Consumes = new string[] { },
+            Produces = new string[] { })]
+        [SwaggerResponse(200, "Successfully returned options in http header", null)]
         [HttpOptions]
         public IActionResult GetGenresOptions()
         {
@@ -166,10 +232,16 @@ namespace Biob.Web.Api.Controllers
             return Ok();
         }
 
+        [SwaggerOperation(
+            Summary = "Get option information",
+            Description = "Gets HTTP methods options for this route",
+            Consumes = new string[] { },
+            Produces = new string[] { })]
+        [SwaggerResponse(200, "Successfully returned options in http header", null)]
         [HttpOptions("{genreId}")]
         public IActionResult GetGenreOptions()
         {
-            Response.Headers.Add("Allow", "GET,PATCH,PUT,OPTIONS");
+            Response.Headers.Add("Allow", "GET,PATCH,PUT,OPTIONS,DELETE");
             return Ok();
         }
 
